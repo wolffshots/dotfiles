7bd55157ce106f09e7d19043fe015d2408917b45
Jadon <10365495-wolffs@users.noreply.gitlab.com>
2023-03-14 09:32:01 +0200

    chore: updated vars and added functions

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,3 +1,3 @@
-function config --wraps='git --git-dir=$HOME/.cfg/ --work-tree=$HOME' --description 'alias config=git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-  git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv; 
+function config --wraps='git' --description 'manage dotfile config'
+  git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $argv; 
 end
---
+++ .config/fish/functions/git1.fish
@@ -0,0 +1,4 @@
+function git1 --wraps='git clone --depth 1' --description 'alias git1 git clone --depth 1'
+  git clone --depth 1 $argv
+        
+end
