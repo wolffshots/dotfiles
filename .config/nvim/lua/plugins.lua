99318d5acd6a06af96de4ec7794f107bdce687f1
Jadon <10365495-wolffs@users.noreply.gitlab.com>
2024-06-04 15:06:44 +0200

    chore: add initial config for all plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -41,30 +41,30 @@ require("lazy").setup({
                 version = "v2.*",
         },
     "tanvirtin/monokai.nvim",
-    {
-        "ray-x/go.nvim",
-        dependencies = {  -- optional packages
-            "ray-x/guihua.lua",
-            "neovim/nvim-lspconfig",
-            "nvim-treesitter/nvim-treesitter",
-        },
-        config = function()
-        require("go").setup()
-        end,
-        event = {"CmdlineEnter"},
-        ft = {"go", 'gomod'},
-        build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-    },
-    {
-        'Exafunction/codeium.vim',
-        config = function ()
-            -- Change '<C-g>' here to any keycode you like.
-            vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-            vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-            vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-            vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-        end
-    },
+    -- {
+    --     "ray-x/go.nvim",
+    --     dependencies = {  -- optional packages
+    --         "ray-x/guihua.lua",
+    --         "neovim/nvim-lspconfig",
+    --         "nvim-treesitter/nvim-treesitter",
+    --     },
+    --     config = function()
+    --     require("go").setup()
+    --     end,
+    --     event = {"CmdlineEnter"},
+    --     ft = {"go", 'gomod'},
+    --     build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    -- },
+    -- {
+    --     'Exafunction/codeium.vim',
+    --     config = function ()
+    --         -- Change '<C-g>' here to any keycode you like.
+    --         vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+    --         vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+    --         vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+    --         vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    --     end
+    -- },
     {
                 "folke/noice.nvim",
                 event = "VeryLazy",
@@ -85,49 +85,49 @@ require("lazy").setup({
     -- Git decorations
         {
                 "lewis6991/gitsigns.nvim",
-                -- config = function()
-                --      require("config.gitsigns")
-                -- end,
+                config = function()
+                     require("config.gitsigns")
+                end,
         },
     -- Autopairs: [], (), "", '', etc
         {
                 "windwp/nvim-autopairs",
                 event = "InsertEnter",
-                -- config = function()
-                --      require("config.nvim-autopairs")
-                -- end,
+                config = function()
+                     require("config.nvim-autopairs")
+                end,
         },
     -- Treesitter-integration
         {
                 "nvim-treesitter/nvim-treesitter",
                 build = ":TSUpdate",
-                -- config = function()
-                --      require("config.nvim-treesitter")
-                -- end,
+                config = function()
+                     require("config.nvim-treesitter")
+                end,
         },
         -- Nvim-treesitter text objects
         {
                 "nvim-treesitter/nvim-treesitter-textobjects",
                 dependencies = "nvim-treesitter/nvim-treesitter",
-                -- config = function()
-                --      require("config.nvim-treesitter-textobjects")
-                -- end,
+                config = function()
+                     require("config.nvim-treesitter-textobjects")
+                end,
         },
         -- Show indentation and blankline
         {
                 "lukas-reineke/indent-blankline.nvim",
                 main = "ibl",
-                -- config = function()
-                --      require("config.indent-blankline")
-                -- end,
+                config = function()
+                     require("config.indent-blankline")
+                end,
         },
         -- Status line
         {
                 "nvim-lualine/lualine.nvim",
                 dependencies = { "nvim-tree/nvim-web-devicons" },
-                -- config = function()
-                --      require("config.lualine")
-                -- end,
+                config = function()
+                     require("config.lualine")
+                end,
         },
     -- Markdown support
         { "preservim/vim-markdown", ft = { "markdown" } },
@@ -137,7 +137,7 @@ require("lazy").setup({
                 cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
                 ft = { "markdown" },
                 build = function()
-                        vim.fn["mkdp#util#install"]()
+                        vim.fn["mkdp#util#install"]() -- you may need to run `call mkdp#util#install()` yourself if it doesn't work immediately
                 end,
         },
         -- File explorer
@@ -146,26 +146,26 @@ require("lazy").setup({
                 dependencies = {
                         "nvim-tree/nvim-web-devicons", -- optional, for file icons
                 },
-                -- config = function()
-                --      require("config.nvim-tree")
-                -- end,
+                config = function()
+                     require("config.nvim-tree")
+                end,
         },
     -- Better terminal integration
         {
                 "akinsho/toggleterm.nvim",
                 version = "*",
-                -- config = function()
-                --       require("config.toggleterm")
-                -- end,
+                config = function()
+                      require("config.toggleterm")
+                end,
         },
     -- Fuzzy finder
         {
                 "nvim-telescope/telescope.nvim",
                 branch = "0.1.x",
                 dependencies = { "nvim-lua/plenary.nvim" },
-                -- config = function()
-                --      require("config.telescope")
-                -- end,
+                config = function()
+                     require("config.telescope")
+                end,
         },
     {
                 "folke/trouble.nvim",
@@ -203,13 +203,8 @@ require("lazy").setup({
                                 desc = "Quickfix List (Trouble)",
                         },
                 },
-                -- opts = function()
-                --      require("config.trouble")
-                -- end,
-        },
-        {
-            "junegunn/fzf",
-            dir = "~/.fzf",
-            build = "./install --all",
+                opts = function()
+                     require("config.trouble")
+                end,
         },
 })
